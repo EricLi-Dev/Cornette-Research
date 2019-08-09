51dc1cb76e158e7069f83fd402276d12606ff186
TJ Ledwith <makertech81@gmail.com>
2019-08-08 23:16:17 -0400

    Update cornette.py

--- cornette.py
+++ cornette.py
@@ -1,1 +1,11 @@
-printf("hello, world!")
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
